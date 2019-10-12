7a1f3eb7d341f2f785374258075bfe298a84f852
Erayus <raymondhieutran@gmail.com>
2019-10-12 12:01:21 +1100

    [New] Added charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { WritingComponent } from './writing/writing.component';
 import { ReadingComponent } from './reading/reading.component';
 import { ListeningComponent } from './listening/listening.component';
 import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './profile/line-chart/line-chart.component';
+import { RadarChartComponent } from './profile/radar-chart/radar-chart.component';
+import { PieChartComponent } from './profile/pie-chart/pie-chart.component';
+import { BarChartComponent } from './profile/bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { ChartsModule } from 'ng2-charts';
     SpeakingComponent,
     WritingComponent,
     ReadingComponent,
-    ListeningComponent
+    ListeningComponent,
+    LineChartComponent,
+    RadarChartComponent,
+    PieChartComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
