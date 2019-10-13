6b04c18e620a3a39657e8d697938b6cf7f32cfd0
Erayus <raymondhieutran@gmail.com>
2019-10-14 00:26:14 +1100

    [New] Complete Speaking Logic Code

--- src/app/speaking/speaking.component.ts
+++ src/app/speaking/speaking.component.ts
@@ -9,29 +9,56 @@ declare var webkitSpeechRecognition: any;
 export class SpeakingComponent implements OnInit {
   recognition: any;
   $result: any;
+  testMode = 'start';
   isRecording = false;
   prepareDuration = 40;
   recordingDuration = 40;
+  prepareInterval;
+  recordingInterval;
   constructor() {}
 
   ngOnInit() {
     this.$result = document.getElementById('result');
   }
-  startTest(){
-    let prepareInterval = setInterval(() => {
+  triggerTest(){
+    switch (this.testMode) {
+      case 'start':
+        this.testMode = 'stop';
+        this.startTimer();
+        break;
+      case 'stop':
+        this.testMode = 'retry';
+        if (this.recognition) {
+          this.isRecording = false;
+          this.recognition.stop();
+          clearInterval(this.recordingInterval);
+        }
+        clearInterval(this.prepareInterval);
+        break;
+      case 'retry':
+        this.testMode = 'stop';
+        this.prepareDuration = 3;
+        this.startTimer();
+        break;
+    }
+
+  }
+  startTimer(){
+    this.prepareInterval = setInterval(() => {
       if (this.prepareDuration > 0) {
         this.prepareDuration -= 1;
       } else {
-        clearInterval(prepareInterval);
+        clearInterval(this.prepareInterval);
         this.startRecording();
       }
-    }, 1000)
+    }, 1000);
   }
 
   startRecording(){
     if ("webkitSpeechRecognition" in window) {
     navigator.mediaDevices.getUserMedia({ audio: true })
           .then((stream) => {
+              this.isRecording = true;
               this.recognition = new webkitSpeechRecognition();
               this.recognition.continuous = true;
               this.recognition.interimResults = true;
@@ -52,11 +79,12 @@ export class SpeakingComponent implements OnInit {
               };
       });
     }
-    let recordingInterval = setInterval(() => {
+    this.recordingDuration = 40;
+    this.recordingInterval = setInterval(() => {
       if (this.recordingDuration > 0) {
         this.recordingDuration -= 1;
       } else {
-        clearInterval(recordingInterval);
+        clearInterval(this.recordingInterval);
         this.recognition.stop();
         this.isRecording = false;
       }
