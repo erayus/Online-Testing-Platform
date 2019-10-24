fbc3e8a87fb9e70ce32c1e37bc9cbe3c69d6a960
Erayus <raymondhieutran@gmail.com>
2019-10-24 23:44:35 +1100

    [New] Get Audio Length

--- src/app/speaking/repeat-sentence/repeat-sentence.component.ts
+++ src/app/speaking/repeat-sentence/repeat-sentence.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-repeat-sentence',
   templateUrl: './repeat-sentence.component.html',
   styleUrls: ['./repeat-sentence.component.css']
 })
 export class RepeatSentenceComponent implements OnInit {
-
-  @ViewChild('audioPlayer',{static: true})
-  audioPlayerRef: ElementRef;
 
   audioSrc: string;
   answerScript: string;
@@ -18,11 +15,19 @@ export class RepeatSentenceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let audio = new Audio();
+    $(audio).on("loadedmetadata", function(){
+      var val = audio.duration;
+
+      return val;
+     });
+    audio.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/15309/test.mp3';
+    console.log(audio.duration);
     this.preparationInterval = setInterval(() => {
       this.timeLeft -=1;
       if (this.timeLeft == 0){
         clearInterval(this.preparationInterval);
-        this.audioPlayerRef.nativeElement.play();
+        audio.play()
       }
     }, 1000);
     this.answerScript = 'This is an audio recording. You are hearing the audio';
