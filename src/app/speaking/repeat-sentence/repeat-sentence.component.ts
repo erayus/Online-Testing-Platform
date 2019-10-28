ca6b97d3a7bb93b28589167ab7f74edd4ea44659
Erayus <raymondhieutran@gmail.com>
2019-10-28 20:33:46 +1100

    [New] Added Recording Status UI

--- src/app/speaking/repeat-sentence/repeat-sentence.component.ts
+++ src/app/speaking/repeat-sentence/repeat-sentence.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 declare var $: any;
+declare var webkitSpeechRecognition: any;
+
+
 @Component({
   selector: 'app-repeat-sentence',
   templateUrl: './repeat-sentence.component.html',
@@ -10,8 +13,16 @@ export class RepeatSentenceComponent implements OnInit {
   answerScript: string;
   userAnswer = '';
   timeLeft = 3;
+  recordingDuration = 40;
   preparationInterval;
   audioProgress: number;
+
+  isRecording = false;
+  recognition: any;
+  userInput: any;
+  testingPassage: any;
+
+
   constructor() { }
   ngOnInit() {
     const audio = new Audio();
@@ -21,7 +32,7 @@ export class RepeatSentenceComponent implements OnInit {
         this.timeLeft -=1;
         if (this.timeLeft === 0){
           clearInterval(this.preparationInterval);
-          audio.play()
+          audio.play();
           this.updateAudioProgressBar(totalDuration);
         }
       }, 1000);
@@ -36,9 +47,70 @@ export class RepeatSentenceComponent implements OnInit {
     const audioDurationInterval = setInterval(() => {
       currentDuration -= 1;
       this.audioProgress = 100 - Math.floor(currentDuration / initialDuration * 100);
-      if ( currentDuration === 0 ) {
+      if ( currentDuration === 0 ) { // After the audio is finished playing
         clearInterval(audioDurationInterval);
       }
     }, 1000);
+  };
+
+  startRecording() {
+    if ("webkitSpeechRecognition" in window) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+            .then((stream) => {
+                this.isRecording = true;
+                this.recognition = new webkitSpeechRecognition();
+                this.recognition.continuous = true;
+                this.recognition.interimResults = true;
+
+                this.recognition.start();
+                this.userInput = '';
+
+                this.recognition.onresult = (event) => {
+                  for (let i = event.resultIndex; i < event.results.length; ++i) {
+                    if (event.results[i].isFinal) {
+                      this.userInput += event.results[i][0].transcript;
+                      // this.$result.innerHTML = finalScripts;
+                      // console.log(this.result);
+                    }
+                  }
+                };
+                this.recordingDuration = 40;
+                let recordingInterval = setInterval(() => {
+                  if (this.recordingDuration > 0) {
+                    this.recordingDuration -= 1;
+                  } else {
+                    clearInterval(recordingInterval);
+                    // this.testMode = 'retry';
+                    this.isRecording = false;
+                    this.recognition.stop();
+                    setTimeout(() => {
+                      this.compare(this.userInput);
+                    }, 2000);
+
+                  }
+                }, 1000);
+        });
+
+      }
   }
+  compare(userInput: string){
+    console.log("User Input:", userInput);
+    const splitPassage = this.testingPassage.toLowerCase().replace(/\.|\,|"/g, "").split(' ');
+    console.log('Split Passage: ', splitPassage);
+    const splitUserInput = userInput.split(' ');
+    console.log('Split User InputL ', splitUserInput);
+    const total = splitPassage.length;
+    let score = 0;
+
+    // Loop through each word of the user input
+      // Loop through the testing passage from the "Correct Word Index"
+      // If not correct, continue
+      // If correct:
+        // Increase the score
+    console.log('Score: ', score);
+    // this.result = Math.ceil(score / total * 100 );
+    // this.showResult = true;
+    // console.log('Result: ', this.result);
+  }
+
 }
